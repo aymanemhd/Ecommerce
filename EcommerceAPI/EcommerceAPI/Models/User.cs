a13fea82b42178d1f6d80c3bfb5fe76830d034cb
aymanemhd <aymane.199820.mehdi@gmail.com>
2023-10-06 21:07:23 +0200

    Add Models

---
+++ EcommerceAPI/EcommerceAPI/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce_API.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
